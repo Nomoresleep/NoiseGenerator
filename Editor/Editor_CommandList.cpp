4aa7ca332d77a3be1ea136e514f629b76163fcf5
Nomoresleep <38327728+Nomoresleep@users.noreply.github.com>
2018-05-14 09:37:37 +0200

    Command list stack discard

--- Editor/Editor_CommandList.cpp
+++ Editor/Editor_CommandList.cpp
@@ -8,16 +8,19 @@ Editor_CommandList::Editor_CommandList()
 
 void Editor_CommandList::ExecuteCommand(Editor_Command* aCommand)
 {
-	if (myCommandIndexPointer == myCommands.Count())
-	{
-		myCommands.Add(aCommand);
-		aCommand->Execute();
-		myCommandIndexPointer = myCommands.Count();
-	}
-	else
-	{
-		assert(false);
-	}
+    if (myCommandIndexPointer != myCommands.Count())
+    {
+        //TODO: Nodes that have been removed by a command can be deleted when the command itself is removed by command stack discard
+        for (s32 commandIdx = myCommandIndexPointer; commandIdx < myCommands.Count(); ++commandIdx)
+        {
+            delete myCommands[commandIdx];
+        }
+        myCommands.RemoveNAtEnd(myCommands.Count() - myCommandIndexPointer);
+    }
+
+    myCommands.Add(aCommand);
+    aCommand->Execute();
+    myCommandIndexPointer = myCommands.Count();
 }
 
 void Editor_CommandList::Undo()
